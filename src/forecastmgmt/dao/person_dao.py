043c2878f4f95053a7b91a0a579cbafb5214f48c
vvladych <vvladych@vvladych.cr>
2014-10-10 00:26:47 +0200

    insert functionality added to the personDAO

--- src/forecastmgmt/dao/person_dao.py
+++ src/forecastmgmt/dao/person_dao.py
@@ -2,17 +2,37 @@
 __author__="vvladych"
 __date__ ="$08.10.2014 06:04:16$"
 
-from db_connection import get_db_connection
+from forecastmgmt.dao.db_connection import get_db_connection
+from forecastmgmt.model.person import Person
+import psycopg2.extras
 
 
 class PersonDAO:
     
     def __init__(self):
-        self.personlist=[]
+        self.sql_select_dict={"get_all_persons":"SELECT sid, common_name FROM fc_person"}
+        self.sql_insert_dict={"insert_person":"INSERT INTO person(common_name) VALUES(%(str)s) RETURNING sid"}
+        
         
     def get_all_persons(self):
-        cur = db_connection.get_db_connection().cursor()
-        cur.execute("SELECT sid, common_name, person_uuid FROM person")
-        persons = cur.fetchall()
+        personlist=[]
+        cur = get_db_connection().cursor(cursor_factory=psycopg2.extras.NamedTupleCursor)
+        cur.execute(self.sql_select_dict["get_all_persons"])
+        for person in cur.fetchall():
+            personlist.append(Person(person.sid, person.common_name))
         cur.close()
+        return personlist
     
+    
+    def insert_person(self, person):
+        cur = get_db_connection().cursor()
+        cur.execute(self.sql_insert_dict,(person.get_common_name(),))
+        person.set_sid(cursor.fetchone()[0])
+    
+        
+    def get_uuid_from_sid(self,sid):
+        pass
+    
+    
+    def delete_person(self, Person):
+        sql_del="DELETE FROM fc_person WHERE sid=%s"
