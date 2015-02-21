52c8ed6c17af545becc4747c349014bfb0732842
VVladych <vvladych@yahoo.de>
2015-02-21 23:02:11 +0100

    common name added to the person add mask

--- src/forecastmgmt/ui/person_add_mask.py
+++ src/forecastmgmt/ui/person_add_mask.py
@@ -3,6 +3,7 @@ from gi.repository import Gtk
 from forecastmgmt.dao.person_dao import PersonDAO
 from forecastmgmt.dao.person_name_dao import get_person_name_roles
 from forecastmgmt.dao.namepart_dao import get_name_part_roles
+from forecastmgmt.model.person import Person
 
 class PersonAddMask(Gtk.Grid):
     def __init__(self, reset_callback, main_window):
@@ -15,69 +16,83 @@ class PersonAddMask(Gtk.Grid):
         placeholder_label = Gtk.Label("")
         placeholder_label.set_size_request(1,40)
         self.attach(placeholder_label,0,-1,1,1)
+
+        row = 0
         # Row 0: person uuid
         uuid_label = Gtk.Label("person UUID")
-        self.attach(uuid_label,0,0,1,1)
+        self.attach(uuid_label,0,row,1,1)
         uuid_text_entry=Gtk.Entry()
-        self.attach(uuid_text_entry,1,0,1,1)
+        self.attach(uuid_text_entry,1,row,1,1)
+        
+        row+=1
+        # Row 1: common name
+        common_name_label = Gtk.Label("Common Name")
+        self.attach(common_name_label,0,row,1,1)
+        self.common_name_text_entry=Gtk.Entry()
+        self.attach(self.common_name_text_entry,1,row,1,1)
 
-        # Row 1: name 
+        row+=1
+        # Row 2: name 
         name_label = Gtk.Label("Name")
-        self.attach(name_label,0,1,1,1)
+        self.attach(name_label,0,row,1,1)
 
         self.name_roles_model=self.populate_name_roles_model()
         self.name_role_combobox=Gtk.ComboBox.new_with_model_and_entry(self.name_roles_model)
         self.name_role_combobox.set_entry_text_column(1)
         self.name_role_combobox.set_active(0)
-        self.attach(self.name_role_combobox,1,1,1,1)
+        self.attach(self.name_role_combobox,1,row,1,1)
 
         name_add_button = Gtk.Button("Add", Gtk.STOCK_ADD)
         name_add_button.connect("clicked", self.add_name)
-        self.attach(name_add_button,2,1,1,1)
+        self.attach(name_add_button,2,row,1,1)
 
         name_delete_button = Gtk.Button("Delete", Gtk.STOCK_DELETE)
         name_delete_button.connect("clicked", self.delete_name)
-        self.attach(name_delete_button,3,1,1,1)
+        self.attach(name_delete_button,3,row,1,1)
         
+        row+=1
         # Row 2: name part role
         namepart_label = Gtk.Label("Name part")
-        self.attach(namepart_label,0,2,1,1)
+        self.attach(namepart_label,0,row,1,1)
 
         namepart_add_button = Gtk.Button("Add", Gtk.STOCK_ADD)
         namepart_add_button.connect("clicked", self.add_name_part) 
-        self.attach(namepart_add_button,2,2,1,1)
+        self.attach(namepart_add_button,2,row,1,1)
 
         namepart_add_button = Gtk.Button("Delete", Gtk.STOCK_DELETE)
         namepart_add_button.connect("clicked", self.delete_name_part) 
-        self.attach(namepart_add_button,3,2,1,1)
+        self.attach(namepart_add_button,3,row,1,1)
 
         self.namepart_roles_model = self.populate_namepart_roles_model()
         self.namepart_role_combobox=Gtk.ComboBox.new_with_model_and_entry(self.namepart_roles_model)
         self.namepart_role_combobox.set_entry_text_column(1)
         self.namepart_role_combobox.set_active(0)
 
-        self.attach(self.namepart_role_combobox,1,2,1,1)
+        self.attach(self.namepart_role_combobox,1,row,1,1)
 
+        row+=1
         # Row 3: name part value
         self.namepart_role_value_entry = Gtk.Entry()
-        self.attach(self.namepart_role_value_entry,1,3,1,1)
+        self.attach(self.namepart_role_value_entry,1,row,1,1)
 
+        row+=1
         # Row 4: treeview 
         self.create_namepart_treeview()
-        self.attach(self.nameparts_treeview,0,4,3,1)
+        self.attach(self.nameparts_treeview,0,row,3,1)
 
+        row+=1
         # Row 5
         save_button = Gtk.Button("save", Gtk.STOCK_SAVE)
         save_button.connect("clicked", self.save_person)
-        self.attach(save_button,1,5,1,1)
+        self.attach(save_button,1,row,1,1)
 
         new_button = Gtk.Button("New", Gtk.STOCK_NEW)
         new_button.connect("clicked", self.new_person_func)
-        self.attach(new_button,2,5,1,1)
+        self.attach(new_button,2,row,1,1)
 
         cancel_button = Gtk.Button("Cancel", Gtk.STOCK_CANCEL)
         cancel_button.connect("clicked", self.parent_callback_func, reset_callback)
-        self.attach(cancel_button,3,5,1,1)
+        self.attach(cancel_button,3,row,1,1)
 
     def add_name(self,widget):
         name_role_id,name_role_value = self.get_active_name_role()
@@ -100,7 +115,10 @@ class PersonAddMask(Gtk.Grid):
         print("new person: unimplemented")
 
     def save_person(self, widget):
-        print("save person: unimplemented")
+        person=Person(None, self.common_name_text_entry.get_text())
+        personDAO=PersonDAO()
+        personDAO.insert(person)
+        print("person added, sid is: %s" % person.sid)
 
     def get_active_name_treestore(self):
         model,tree_iter=self.nameparts_treeview.get_selection().get_selected()
