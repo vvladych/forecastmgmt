b88e2fd411ddb86d63abade19c05c96716dc51e3
VVladych <vvladych@yahoo.de>
2015-02-23 00:24:24 +0100

    added birth_place and birth_date to the person object

--- src/forecastmgmt/dao/person_dao.py
+++ src/forecastmgmt/dao/person_dao.py
@@ -10,8 +10,8 @@ import psycopg2.extras
 class PersonDAO:
     
     def __init__(self):
-        self.sql_select_dict={"get_all_persons":"SELECT sid, common_name FROM fc_person"}
-        self.sql_insert_dict={"insert_person":"INSERT INTO fc_person(common_name) VALUES(%s) RETURNING sid"}
+        self.sql_select_dict={"get_all_persons":"SELECT sid, common_name, birth_date, birth_place, person_uuid FROM fc_person"}
+        self.sql_insert_dict={"insert_person":"INSERT INTO fc_person(common_name, birth_date, birth_place) VALUES(%s,%s,%s) RETURNING sid"}
         
         
     def get_all_persons(self):
@@ -19,14 +19,14 @@ class PersonDAO:
         cur = get_db_connection().cursor(cursor_factory=psycopg2.extras.NamedTupleCursor)
         cur.execute(self.sql_select_dict["get_all_persons"])
         for person in cur.fetchall():
-            personlist.append(Person(person.sid, person.common_name))
+            personlist.append(Person(person.sid, person.common_name, person.birth_date, person.birth_place, person.person_uuid))
         cur.close()
         return personlist
     
     
     def insert(self, person):
         cur = get_db_connection().cursor()
-        data=(person.common_name,)
+        data=(person.common_name,person.birth_date,person.birth_place)
         cur.execute(self.sql_insert_dict["insert_person"],data)
         person.sid=cur.fetchone()[0]
     
--- src/forecastmgmt/model/person.py
+++ src/forecastmgmt/model/person.py
@@ -5,9 +5,12 @@ __date__ ="$09.10.2014 23:01:15$"
 class Person:
     
     def __init__(self):
-        self(None,None,None)
+        self(None,None,None,None,None,None)
         
-    def __init__(self, sid, common_name):
+    def __init__(self, sid, common_name, birth_date, birth_place, person_uuid):
         self.sid=sid
         self.common_name=common_name
+        self.birth_date=birth_date
+        self.birth_place=birth_place
+        self.person_uuid=person_uuid
     
