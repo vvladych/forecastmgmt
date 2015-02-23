16f61d0f8d4f7d4cdd4083fa1f054f670e168409
VVladych <vvladych@yahoo.de>
2015-02-24 00:13:23 +0100

    made treeview member

--- src/forecastmgmt/ui/person_list_mask.py
+++ src/forecastmgmt/ui/person_list_mask.py
@@ -9,24 +9,24 @@ class PersonListMask(Gtk.Box):
     def __init__(self):
         Gtk.Box.__init__(self, orientation=Gtk.Orientation.VERTICAL)
         self.store = Gtk.ListStore(str,str,str,str,bool,str)        
-        tree = Gtk.TreeView(self.store)  
+        self.tree = Gtk.TreeView(self.store)  
         # add person's common name              
-        tree.append_column(add_column_to_treeview("common name", 0, False))
-        tree.append_column(add_column_to_treeview("birth date", 1, False))
-        tree.append_column(add_column_to_treeview("birth place", 2, False))
-        tree.append_column(add_column_to_treeview("person uuid", 3, False))
+        self.tree.append_column(add_column_to_treeview("common name", 0, False))
+        self.tree.append_column(add_column_to_treeview("birth date", 1, False))
+        self.tree.append_column(add_column_to_treeview("birth place", 2, False))
+        self.tree.append_column(add_column_to_treeview("person uuid", 3, False))
         
         
         renderer_toggle=Gtk.CellRendererToggle()
         renderer_toggle.connect("toggled", self.on_cell_toggled)
         column_toggle=Gtk.TreeViewColumn("delete", renderer_toggle, active=4)
-        tree.append_column(column_toggle)
+        self.tree.append_column(column_toggle)
         
-        tree.append_column(add_column_to_treeview("person_sid", 5, True))
+        self.tree.append_column(add_column_to_treeview("person_sid", 5, True))
         
         
-        tree.set_size_request(200,300)
-        self.pack_start(tree, False, False, 0)        
+        self.tree.set_size_request(200,300)
+        self.pack_start(self.tree, False, False, 0)        
         self.populate_person_view_table()
         
     
@@ -52,3 +52,8 @@ class PersonListMask(Gtk.Box):
                 PersonDAO().delete(Person(person_sid))
                 self.store.remove(iter)
             iter=self.store.iter_next(iter)
+            
+    def get_current_person(self):
+        model,tree_iter = self.tree.get_selection().get_selected()
+        (person_sid)=self.store.get(tree_iter, 5)
+        return Person(person_sid)
