1222b08ea74c4325ac86b451e72665eec85decb4
VVladych <vvladych@yahoo.de>
2015-02-23 23:02:55 +0100

    constructors refactored

--- src/forecastmgmt/model/person.py
+++ src/forecastmgmt/model/person.py
@@ -4,10 +4,8 @@ __date__ ="$09.10.2014 23:01:15$"
 
 class Person:
     
-    def __init__(self):
-        self(None,None,None,None,None,None)
         
-    def __init__(self, sid, common_name, birth_date, birth_place, person_uuid):
+    def __init__(self, sid=None, common_name=None, birth_date=None, birth_place=None, person_uuid=None):
         self.sid=sid
         self.common_name=common_name
         self.birth_date=birth_date
