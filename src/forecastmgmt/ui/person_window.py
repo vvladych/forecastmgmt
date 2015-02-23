80ac856aae15fc1ce42edb5f4e3d1dd89a097eeb
VVladych <vvladych@yahoo.de>
2015-02-23 22:34:26 +0100

    removed list action area

--- src/forecastmgmt/ui/person_window.py
+++ src/forecastmgmt/ui/person_window.py
@@ -28,24 +28,19 @@ class PersonWindow(Gtk.Box):
             self.delete_person_button.set_size_request(30,30)
             self.delete_person_button.connect("clicked", self.delete_person, "delete")
             self.person_action_area.pack_start(self.delete_person_button, False, False, 0)
-            
-            
-        elif action=="add":
-            save_new_person=Gtk.Button.new_from_stock(Gtk.STOCK_SAVE)
-            self.person_action_area.pack_start(save_new_person, False, False, 0)
-            cancel_new_person=Gtk.Button.new_from_stock(Gtk.STOCK_CANCEL)
-            cancel_new_person.connect("clicked", self.add_working_area, "list")
-            self.person_action_area.pack_start(cancel_new_person, False, False, 0)
 
         self.person_action_area.show_all()
+        
 
     def clean_working_area_box(self):
         for child in self.person_working_area.get_children():
             self.person_working_area.remove(child)
+            
 
     def clean_action_area_box(self):
         for child in self.person_action_area.get_children():
             self.person_action_area.remove(child)
+            
 
     def add_working_area(self, widget, action="list"):
         self.clean_working_area_box()
@@ -59,6 +54,7 @@ class PersonWindow(Gtk.Box):
 
     def default_view(self):
         self.add_working_area("list")
+        
         
     def delete_person(self,widget,callback):
         confirm_dialog=DeletePersonConfirmationDialog(self.main_window)
