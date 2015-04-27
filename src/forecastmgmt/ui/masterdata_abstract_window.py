b3bc2369b974063fa15bee44de826543850bab40
VVladych <vvladych@yahoo.de>
2015-04-28 01:01:09 +0200

    masterdata: abstract window, implementation for person

---
+++ src/forecastmgmt/ui/masterdata_abstract_window.py
@@ -0,0 +1,52 @@
+'''
+Created on 27.04.2015
+
+@author: vvladych
+'''
+
+
+from gi.repository import Gtk
+
+class MasterdataAbstractWindow(Gtk.Box):
+    
+    def __init__(self, main_window):
+        Gtk.Box.__init__(self, orientation=Gtk.Orientation.VERTICAL)
+        self.main_window=main_window
+        
+        self.action_area = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+        self.pack_start(self.action_area, False, False, 0)
+        self.working_area=Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        self.pack_start(self.working_area, False, False, 0)
+        
+        
+    def clean_working_area_box(self):
+        for child in self.working_area.get_children():
+            self.working_area.remove(child)
+
+            
+    def clean_action_area_box(self):
+        for child in self.action_area.get_children():
+            self.action_area.remove(child)
+            
+    def create_action_area(self):
+        self.add_new_button=Gtk.Button.new_from_stock(Gtk.STOCK_ADD)
+        self.add_new_button.set_size_request(30,30)
+        self.add_new_button.connect("clicked", self.add_working_area, "add")
+        self.action_area.pack_start(self.add_new_button, False, False, 0)
+
+        self.edit_button=Gtk.Button.new_from_stock(Gtk.STOCK_EDIT)
+        self.edit_button.set_size_request(30,30)
+        self.edit_button.connect("clicked", self.add_working_area, "edit")
+        self.action_area.pack_start(self.edit_button, False, False, 0)
+
+        
+        self.delete_button=Gtk.Button.new_from_stock(Gtk.STOCK_DELETE)
+        self.delete_button.set_size_request(30,30)
+        self.delete_button.connect("clicked", self.delete_action, "delete")
+        self.action_area.pack_start(self.delete_button, False, False, 0)
+
+        self.action_area.show_all()
+        
+    def delete_action(self):
+        raise NotImplementedError("Delete action not implemented")
+
--- src/forecastmgmt/ui/person_window.py
+++ src/forecastmgmt/ui/person_window.py
@@ -1,78 +1,41 @@
 from gi.repository import Gtk
 
+from masterdata_abstract_window import MasterdataAbstractWindow
+
 from person_add_mask import PersonAddMask
 from person_list_mask import PersonListMask
 
-class PersonWindow(Gtk.Box):
+class PersonWindow(MasterdataAbstractWindow):
 
     def __init__(self, main_window):
-        Gtk.Box.__init__(self, orientation=Gtk.Orientation.VERTICAL)
-        self.main_window=main_window
-        self.person_action_area = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
-        self.pack_start(self.person_action_area, False, False, 0)
-        self.person_working_area=Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-        self.pack_start(self.person_working_area, False, False, 0)
-        
+        super(PersonWindow, self).__init__(main_window)
         self.add_working_area(None, "list")
 
-    def create_action_area(self, action="list"):
-        if action=="list":
-            self.add_new_person_button=Gtk.Button.new_from_stock(Gtk.STOCK_ADD)
-            self.add_new_person_button.set_size_request(30,30)
-            self.add_new_person_button.connect("clicked", self.add_working_area, "add")
-            self.person_action_area.pack_start(self.add_new_person_button, False, False, 0)
-
-            self.edit_person_button=Gtk.Button.new_from_stock(Gtk.STOCK_EDIT)
-            self.edit_person_button.set_size_request(30,30)
-            self.edit_person_button.connect("clicked", self.add_working_area, "edit")
-            self.person_action_area.pack_start(self.edit_person_button, False, False, 0)
-
-            
-            self.delete_person_button=Gtk.Button.new_from_stock(Gtk.STOCK_DELETE)
-            self.delete_person_button.set_size_request(30,30)
-            self.delete_person_button.connect("clicked", self.delete_person, "delete")
-            self.person_action_area.pack_start(self.delete_person_button, False, False, 0)
-
-        self.person_action_area.show_all()
         
-            
-
-    def clean_action_area_box(self):
-        for child in self.person_action_area.get_children():
-            self.person_action_area.remove(child)
-            
-
     def add_working_area(self, widget, action="list"):
         self.clean_working_area_box()
         self.clean_action_area_box()
-        self.create_action_area(action)
+        self.create_action_area()
         if action=="list":
             self.personListMask=PersonListMask()
-            self.person_working_area.pack_start(self.personListMask, False, False, 0)
+            self.working_area.pack_start(self.personListMask, False, False, 0)
         elif action=="add":
-            self.person_working_area.pack_start(PersonAddMask(self.default_view, self.main_window), False, False, 0)
+            self.working_area.pack_start(PersonAddMask(self.default_view, self.main_window), False, False, 0)
         elif action=="edit":
-            person=self.get_current_person()
-            self.person_working_area.pack_start(PersonAddMask(self.default_view, self.main_window, person), False, False, 0)
+            self.working_area.pack_start(PersonAddMask(self.default_view, self.main_window, self.get_current_person()), False, False, 0)
 
-        self.person_working_area.show_all()
+        self.working_area.show_all()   
+
 
     def get_current_person(self):
-        person=self.personListMask.get_current_person()
-        return person
-            
-
-
-    def clean_working_area_box(self):
-        for child in self.person_working_area.get_children():
-            self.person_working_area.remove(child)
+        return self.personListMask.get_current_person()
 
 
     def default_view(self):
         self.add_working_area("list")
         
         
-    def delete_person(self,widget,callback):
+    def delete_action(self,widget,callback):
         confirm_dialog=DeletePersonConfirmationDialog(self.main_window)
         response = confirm_dialog.run()
         if response==Gtk.ResponseType.OK:
@@ -83,8 +46,6 @@ class PersonWindow(Gtk.Box):
         confirm_dialog.destroy()
         
 
-    def reset_callback(self):
-        print("in reset_callback person_window")
         
 class DeletePersonConfirmationDialog(Gtk.Dialog):
     
