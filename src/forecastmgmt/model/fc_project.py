85abb5518bc12dff97c5227b3893573bc049c952
VVladych <vvladych@yahoo.de>
2015-05-31 01:35:56 +0200

    added model_text handling

--- src/forecastmgmt/model/fc_project.py
+++ src/forecastmgmt/model/fc_project.py
@@ -7,12 +7,18 @@ Created on 15.03.2015
 
 from MDO import MDO
 
+from model_text import ModelText
+
+from forecastmgmt.dao.db_connection import get_db_connection
+import psycopg2.extras
+
 class FcProject(MDO):
     sql_dict={  "insert":"INSERT INTO fc_forecast(common_name, short_description) VALUES(%s,%s) RETURNING sid",
                 "delete":"DELETE FROM fc_forecast WHERE sid=%s",
                 "get_all":"SELECT sid, forecast_uuid, common_name, created_date FROM fc_forecast",
                 "get_forecast_models":"SELECT sid, model_date, model_uuid FROM fc_forecast WHERE forecast_sid=%s",
                 "get_forecast_publications":"SELECT",
+                "get_forecast_model_text":"SELECT sid,model_text FROM fc_model_text WHERE forecast_sid=%s",
                 "load":"SELECT forecast_uuid,common_name,created_date,short_description FROM fc_forecast WHERE sid=%s"}
         
     def __init__(self, sid=None, forecast_uuid=None, common_name=None, created_date=None, short_description=None):
@@ -22,6 +28,7 @@ class FcProject(MDO):
         self.short_description=short_description
         if self.sid!=None:
             self._load_project_models()
+            self.load_model_text()
             
 
     def get_insert_data(self):
@@ -43,6 +50,22 @@ class FcProject(MDO):
         
     def _load_project_publications(self):
         self.publications=[]
+        
+    def load_model_text(self):
+        self.model_text=None
+        cur=get_db_connection().cursor(cursor_factory=psycopg2.extras.NamedTupleCursor)
+        data=(self.sid,)
+        cur.execute("""SELECT
+                        sid, model_text 
+                        FROM 
+                        fc_model_text
+                        WHERE
+                        forecast_sid=%s
+                        """,data)
+        for p in cur.fetchall():
+            self.model_text=ModelText(sid=p.sid, model_text=p.model_text, forecast_sid=self.sid)
+        cur.close()        
+
         
     def fabric_method(self,rec):
         return FcProject(rec.sid, rec.forecast_uuid, rec.common_name, rec.created_date)
--- src/forecastmgmt/ui/forecast/model_process_component.py
+++ src/forecastmgmt/ui/forecast/model_process_component.py
@@ -9,6 +9,8 @@ from forecastmgmt.ui.forecast.abstract_data_process_component import AbstractDat
 
 from forecastmgmt.ui.ui_tools import TreeviewColumn, show_info_dialog
 
+from forecastmgmt.model.model_text import ModelText
+
 from forecastmgmt.dao.db_connection import get_db_connection
 import psycopg2.extras
 
@@ -31,6 +33,7 @@ class ModelManipulationComponent(AbstractDataManipulationComponent):
         scrolledwindow.set_hexpand(True)
         scrolledwindow.set_vexpand(True)
         self.forecast_model_textview=Gtk.TextView()
+        self.forecast_model_textview.get_buffer().set_text(self.forecast.model_text.model_text)
         scrolledwindow.add(self.forecast_model_textview)
         parent_layout_grid.attach(scrolledwindow,0,row,1,1)
                 
@@ -39,14 +42,28 @@ class ModelManipulationComponent(AbstractDataManipulationComponent):
         self.save_model_button=Gtk.Button("Save", Gtk.STOCK_SAVE)
         parent_layout_grid.attach(self.save_model_button,1,row,1,1)
         self.save_model_button.connect("clicked", self.save_model_action)
+
         
     def save_model_action(self, widget):
-        pass
+        textbuffer=self.forecast_model_textview.get_buffer()
+        model_text=textbuffer.get_text(textbuffer.get_start_iter(),textbuffer.get_end_iter(),True)
+        if self.forecast.model_text==None:
+            show_info_dialog("insert text")
+            ModelText(forecast_sid=self.forecast.sid, model_text=model_text).insert()
+            self.forecast.load_model_text()
+        else:
+            if self.forecast.model_text.model_text!=model_text:
+                self.forecast.model_text.update(model_text=model_text)
+                show_info_dialog("update model text")
+                self.forecast.load_model_text()
+                self.forecast_model_textview.get_buffer().set_text(self.forecast.model_text.model_text)
+            else:
+                show_info_dialog("nothing to update")
 
     
 class ModelOverviewComponent(AbstractDataOverviewComponent):
     
-    treecolumns=[TreeviewColumn("publication_sid", 0, True)]
+    treecolumns=[TreeviewColumn("model_text_sid", 0, True), TreeviewColumn("model text", 1, False)]
 
     
     def __init__(self, forecast):
@@ -55,4 +72,16 @@ class ModelOverviewComponent(AbstractDataOverviewComponent):
         
 
     def populate_model(self):
-        pass
+        self.treemodel.clear()
+        cur=get_db_connection().cursor(cursor_factory=psycopg2.extras.NamedTupleCursor)
+        data=(self.forecast.sid,)
+        cur.execute("""SELECT
+                        sid, substring(model_text from 1 for 100) as model_text 
+                        FROM 
+                        fc_model_text
+                        WHERE
+                        forecast_sid=%s
+                        """,data)
+        for p in cur.fetchall():
+            self.treemodel.append([ "%s" % p.sid, "%s" % p.model_text, "", "", ""])
+        cur.close()
