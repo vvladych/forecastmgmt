b6904263a5d9530f897c844b74828587d829b31a
VVladych <vvladych@yahoo.de>
2015-06-08 23:48:26 +0200

    added treeview for the object property

--- src/forecastmgmt/ui/masterdata/fc_object_add_mask.py
+++ src/forecastmgmt/ui/masterdata/fc_object_add_mask.py
@@ -8,6 +8,8 @@ from gi.repository import Gtk
 from forecastmgmt.model.fc_object import FCObject
 from masterdata_abstract_window import AbstractAddMask
 
+from forecastmgmt.ui.ui_tools import add_column_to_treeview
+
 
 class FCObjectAddMask(AbstractAddMask):
     def __init__(self, main_window, reset_callback):
@@ -46,7 +48,7 @@ class FCObjectAddMask(AbstractAddMask):
 
         row+=1
         self.create_object_property_treeview()
-        self.attach(self.object_property_treeview,0,row,1,1)
+        self.attach(self.object_property_treeview,0,row,4,1)
 
         row+=3
         
@@ -59,22 +61,40 @@ class FCObjectAddMask(AbstractAddMask):
         back_button.connect("clicked", self.parent_callback_func, self.reset_callback)
         self.attach(back_button,2,row,1,1)
         
-    def add_object_property(self, widget):
-        print("not implemented")
+
+
+    def create_object_property_treeview(self):
+        self.object_property_treestore = Gtk.TreeStore(str)
+        self.object_property_treeview = Gtk.TreeView(self.object_property_treestore)
+        self.object_property_treeview.append_column(add_column_to_treeview("Property", 0, False))
+        self.object_property_treeview.set_size_request(200,300)
+        self.object_property_treeview.connect("row-activated", self.on_row_selection)
+        
+        
+    def on_row_selection(self,treeview,path,column):
+        model,treeiter=self.object_property_treeview.get_selection().get_selected()
+        self.object_property_value_entry.set_text(model[treeiter][0])
+        
+
+    def add_object_property(self,widget):
+        self.object_property_treestore.append(None,[self.object_property_value_entry.get_text()])
+        self.object_property_value_entry.set_text('')
+
 
 
     def delete_object_property(self, widget):
-        print("not implemented")
+        model,tree_iter = self.object_property_treeview.get_selection().get_selected()
+        model.remove(tree_iter)
 
-    def create_object_property_treeview(self):
-        self.object_property_treeview=Gtk.TreeView()
-        print("not implemented")
 
         
     def fill_mask_from_current_object(self):
+        self.object_property_treestore.clear()
         if self.current_object!=None:
             self.uuid_text_entry.set_text(self.current_object.uuid)
             self.common_name_text_entry.set_text(self.current_object.common_name)
+            for object_property in self.current_object.object_properties:
+                self.object_property_treestore.append(None, [object_property.common_name])
         else:
             self.uuid_text_entry.set_text("")
             self.common_name_text_entry.set_text("")
@@ -87,5 +107,14 @@ class FCObjectAddMask(AbstractAddMask):
             self.show_error_dialog("common name cannot be null")
             return
         fc_object=FCObject(None,common_name)
+        
+        # insert object properties
+        object_property_iter=self.object_property_treestore.get_iter_first()
+        while object_property_iter:
+            (object_property_common_name)=self.object_property_treestore.get(object_property_iter, 0)
+            
+            fc_object.add_object_property(None, object_property_common_name, self.current_object.sid)
+            object_property_iter=self.object_property_treestore.iter_next(object_property_iter)
+
         return fc_object
     
