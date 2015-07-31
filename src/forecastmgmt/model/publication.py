f1e41f266a2baceeeade3f4c153a3fff3aaebd14
VVladych <vvladych@yahoo.de>
2015-08-01 00:25:20 +0200

    added publication_text to publication MDO

--- src/forecastmgmt/model/publication.py
+++ src/forecastmgmt/model/publication.py
@@ -10,14 +10,15 @@ class Publication(MDO):
 
     sql_dict={"get_all":"SELECT sid, publisher_sid, uuid, publishing_date, title FROM fc_publication",
               "delete":"DELETE FROM fc_publication WHERE sid=%s",
-              "insert":"INSERT INTO fc_publication(publisher_sid,publishing_date,title) VALUES(%s,%s,%s) RETURNING sid",
+              "insert":"INSERT INTO fc_publication(publisher_sid,publishing_date,title,publication_text) VALUES(%s,%s,%s,%s) RETURNING sid",
               "load":"SELECT title, uuid FROM fc_publication WHERE sid=%s"}
     
-    def __init__(self, sid=None, uuid=None, publisher_sid=None, publishing_date=None, title=None):
+    def __init__(self, sid=None, uuid=None, publisher_sid=None, publishing_date=None, title=None, publication_text=None):
         super(Publication, self).__init__(Publication.sql_dict,sid,uuid)
         self.publisher_sid=publisher_sid
         self.publishing_date=publishing_date
         self.title=title
+        self.publication_text=publication_text
         
 
     def load_object_from_db(self,rec):
@@ -25,10 +26,11 @@ class Publication(MDO):
         self.uuid=rec.uuid
         self.publishing_date=rec.publishing_date
         self.title=rec.title
+        self.publication_text=rec.publication_text
         
     def get_insert_data(self):
-        return (self.publisher_sid,self.publishing_date,self.title,)
+        return (self.publisher_sid,self.publishing_date,self.title,self.publication_text,)
     
     def fabric_method(self,rec):
-        return Publication(rec.sid, rec.uuid, rec.publisher_sid, rec.publisher_date, rec.title)
+        return Publication(rec.sid, rec.uuid, rec.publisher_sid, rec.publisher_date, rec.title, rec.publication_text)
     
