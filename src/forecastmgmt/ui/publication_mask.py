a45724a39742b8cb6e7cebebfe31c1308dbe8266
VVladych <vvladych@yahoo.de>
2015-08-17 23:35:19 +0200

    added new mask for publications

---
+++ src/forecastmgmt/ui/publication_mask.py
@@ -0,0 +1,11 @@
+from gi.repository import Gtk
+
+from abstract_mask import AbstractMask
+
+
+
+class PublicationMask(AbstractMask):
+    
+    def __init__(self, main_window):
+        super(PublicationMask, self).__init__(main_window)
+
