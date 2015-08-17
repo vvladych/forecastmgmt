28f379fd84f415fa07ddc1df3602f0beab594fc7
VVladych <vvladych@yahoo.de>
2015-08-17 23:26:33 +0200

    Refactoring: forecast mask derives from abstract mask

--- src/forecastmgmt/ui/forecast_mask.py
+++ src/forecastmgmt/ui/forecast_mask.py
@@ -12,49 +12,27 @@ from forecastmgmt.ui.forecast.forecast_new_dialog import ForecastNewDialog
 
 from ui_tools import add_column_to_treeview, show_info_dialog
 from forecastmgmt.model.fc_project import FcProject
+from abstract_mask import AbstractMask
 
-class ForecastMask(Gtk.Grid):
+class ForecastMask(AbstractMask):
     
     def __init__(self, main_window):
-        Gtk.Grid.__init__(self)
+        super(ForecastMask, self).__init__(main_window)
 
-        self.main_window=main_window
-        self.project=None
-
-        # Main working pane: contains left pane with actions and working area pane 
-        self.main_working_pane=Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
-        self.main_working_pane.set_size_request(200,600)
-        self.add(self.main_working_pane)
-
-        # the left pane: actions
-        self.main_left_pane = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-
-        # the middle pane: working area
-        self.main_middle_pane = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
-                       
-        self.main_working_pane.pack_start(self.main_left_pane, False, False, 0)
-        self.main_working_pane.pack_start(self.main_middle_pane, False, False, 0)
-        
-        self.create_main_left_pane()
         
         
-    def create_main_left_pane(self):
-        self.__create_forecast_list_treeview()
-        self.main_left_pane.pack_start(self.forecasts_treeview, False, False, 0)  
-        
-        
-    def __create_forecast_list_treeview(self):
+    def create_overview_treeview(self):
         self.forecasts_treestore = Gtk.TreeStore(int,str,str,str)
         self.__populate_forecast_treestore()
-        self.forecasts_treeview = Gtk.TreeView(self.forecasts_treestore)
-        self.forecasts_treeview.append_column(add_column_to_treeview("id", 0, True))
-        self.forecasts_treeview.append_column(add_column_to_treeview("Publisher", 1, False))
-        self.forecasts_treeview.append_column(add_column_to_treeview("Date", 2, False))
-        self.forecasts_treeview.append_column(add_column_to_treeview("Forecast", 3, False))
-        self.__add_context_menu_forecast_treeview()
-        self.forecasts_treeview.set_size_request(200,300)
+        self.overview_treeview = Gtk.TreeView(self.forecasts_treestore)
+        self.overview_treeview.append_column(add_column_to_treeview("id", 0, True))
+        self.overview_treeview.append_column(add_column_to_treeview("Publisher", 1, False))
+        self.overview_treeview.append_column(add_column_to_treeview("Date", 2, False))
+        self.overview_treeview.append_column(add_column_to_treeview("Forecast", 3, False))
         
-    def __add_context_menu_forecast_treeview(self):
+        
+        
+    def add_context_menu_overview_treeview(self):
         menu=Gtk.Menu()
         menu_item_create_new_forecast=Gtk.MenuItem("Add new forecast...")
         menu_item_create_new_forecast.connect("activate", self.on_menu_item_create_new_forecast_click) 
@@ -64,7 +42,7 @@ class ForecastMask(Gtk.Grid):
         menu_item_delete_forecast.connect("activate", self.on_menu_item_delete_forecast_click) 
         menu.append(menu_item_delete_forecast)
         menu_item_delete_forecast.show()
-        self.forecasts_treeview.connect("button_press_event", self.on_treeview_button_press_event,menu)
+        self.overview_treeview.connect("button_press_event", self.on_treeview_button_press_event,menu)
         
         
         
@@ -76,9 +54,9 @@ class ForecastMask(Gtk.Grid):
             new_forecast_dialog.perform_insert()
                 
         elif response==Gtk.ResponseType.CANCEL:
-            print("insert nothing")
+            show_info_dialog("Insert nothing")
         else:
-            print("unknown action")
+            show_info_dialog("Unknown action")
         
         new_forecast_dialog.destroy()
         self.__populate_forecast_treestore()
@@ -89,15 +67,10 @@ class ForecastMask(Gtk.Grid):
         for project in FcProject().get_all():
             self.forecasts_treestore.append(None,[project.sid,"","%s" % project.created_date,project.common_name])
             
-    def __clear_main_middle_pane(self):
-        for child in self.main_middle_pane.get_children():
-            self.main_middle_pane.remove(child)        
              
     
     def on_menu_item_delete_forecast_click(self, widget):
-        print("delete...")
-        (model,tree_iter)=self.forecasts_treeview.get_selection().get_selected()    
-        print("delete forecast_sid: %s" % model.get_value(tree_iter,0))
+        (model,tree_iter)=self.overview_treeview.get_selection().get_selected()    
         fc_project_sid=model.get_value(tree_iter,0)
         nd=Gtk.Dialog("Delete project", self.main_window, 0, ("OK", Gtk.ResponseType.OK, "CANCEL", Gtk.ResponseType.CANCEL))
         ret=nd.run()
@@ -119,7 +92,7 @@ class ForecastMask(Gtk.Grid):
                 project_sid=self.forecasts_treestore.get(self.forecasts_treestore.get_iter(pthinfo[0]),0)
                 self.project=FcProject(project_sid)
                 self.project.load()
-                self.__clear_main_middle_pane()
+                self.clear_main_middle_pane()
                 self.main_middle_pane.pack_start(ForecastOverviewWindow(self,self.project), False, False, 0)
                 self.main_middle_pane.show_all()
         
